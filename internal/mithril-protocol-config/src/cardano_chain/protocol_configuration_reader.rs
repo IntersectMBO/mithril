b120bff0943b8f5f88be165d9f282c8587d11183
Clément Turmel <turmel@teragone-solutions.com>
2026-08-04 09:46:37 +0200

    feature(protocol-config): Implement on-chain reader for protocol configurations

--- internal/mithril-protocol-config/src/cardano_chain/protocol_configuration_reader.rs
+++ internal/mithril-protocol-config/src/cardano_chain/protocol_configuration_reader.rs
@@ -1,34 +1,39 @@
 //! Cardano Chain implementation to read protocol configuration markers
 
 use async_trait::async_trait;
+use mithril_common::entities::Epoch;
+use std::collections::BTreeMap;
 use std::sync::Arc;
 
-use mithril_cardano_node_chain::chain_observer::ChainObserver;
-use mithril_cardano_node_chain::entities::ChainAddress;
-use mithril_common::StdResult;
-use mithril_common::crypto_helper::ProtocolConfigurationMarkersVerifierVerificationKey;
-
+use crate::cardano_chain::message::{
+    ProtocolConfigurationForEpochMessage, ProtocolConfigurationMarker,
+};
+use crate::cardano_chain::payload::SignedProtocolConfigurationMarkersPayload;
 use crate::interface::ProtocolConfigurationMarkersReader;
-use crate::model::ConfigurationResolverFromMarkers;
+use crate::model::{ConfigurationResolverFromMarkers, ProtocolConfigurationForEpoch};
+use mithril_cardano_node_chain::chain_observer::ChainObserver;
+use mithril_cardano_node_chain::entities::{ChainAddress, TxDatumFieldTypeName};
+use mithril_common::crypto_helper::ProtocolConfigurationMarkersVerifierVerificationKey;
+use mithril_common::{StdError, StdResult};
 
 /// Cardano Chain reader retrieves protocol configuration markers on chain
 pub struct CardanoChainProtocolConfigurationMarkersReader {
-    _address: ChainAddress,
-    _chain_observer: Arc<dyn ChainObserver>,
-    _verification_key: ProtocolConfigurationMarkersVerifierVerificationKey,
+    address: ChainAddress,
+    chain_observer: Arc<dyn ChainObserver>,
+    verification_key: ProtocolConfigurationMarkersVerifierVerificationKey,
 }
 
 impl CardanoChainProtocolConfigurationMarkersReader {
     /// CardanoChainAdapter factory
     pub fn new(
-        _address: ChainAddress,
-        _chain_observer: Arc<dyn ChainObserver>,
-        _verification_key: ProtocolConfigurationMarkersVerifierVerificationKey,
+        address: ChainAddress,
+        chain_observer: Arc<dyn ChainObserver>,
+        verification_key: ProtocolConfigurationMarkersVerifierVerificationKey,
     ) -> Self {
         Self {
-            _address,
-            _chain_observer,
-            _verification_key,
+            address,
+            chain_observer,
+            verification_key,
         }
     }
 }
@@ -36,12 +41,173 @@ impl CardanoChainProtocolConfigurationMarkersReader {
 #[async_trait]
 impl ProtocolConfigurationMarkersReader for CardanoChainProtocolConfigurationMarkersReader {
     async fn read_configuration_markers(&self) -> StdResult<ConfigurationResolverFromMarkers> {
-        //read payload
-        // to ProtocolConfigurationForEpochMessage
-        // to ProtocolConfigurationForEpoch
-        // build ConfigurationComputerFromMarkers with ProtocolConfigurationForEpoch
-        Err(anyhow::anyhow!(
-            "CardanoChainProtocolConfigurationMarkersReader::read is not implemented yet"
-        ))
+        let tx_datums = self.chain_observer.get_current_datums(&self.address).await?;
+
+        let markers_list = tx_datums
+            .into_iter()
+            .filter_map(|datum| datum.get_fields_by_type(&TxDatumFieldTypeName::Bytes).ok())
+            .map(|fields| {
+                fields
+                    .iter()
+                    .filter_map(|field_value| field_value.as_str().map(|s| s.to_string()))
+                    .collect::<Vec<String>>()
+                    .join("")
+            })
+            .filter_map(|field_value_str| {
+                SignedProtocolConfigurationMarkersPayload::from_json_hex(&field_value_str).ok()
+            })
+            .filter_map(|markers_payload| {
+                markers_payload
+                    .verify_signature(self.verification_key)
+                    .ok()
+                    .map(|_| markers_payload.markers)
+            })
+            .collect::<Vec<Vec<ProtocolConfigurationMarker>>>();
+
+        let last_markers = markers_list.first().cloned().unwrap_or_default();
+
+        let mut markers_map: BTreeMap<Epoch, ProtocolConfigurationForEpoch> = BTreeMap::new();
+        for marker in last_markers {
+            markers_map.insert(
+                marker.epoch,
+                ProtocolConfigurationForEpochMessage::from_cbor_hex(&marker.configuration)
+                    .map_err(|e| {
+                        StdError::msg(format!(
+                            "ProtocolConfigurationForEpochMessage for Epoch({}) could not be decoded from cbor hex: {}",
+                            marker.epoch,
+                            e
+                        ))
+                    })?
+                    .into(),
+            );
+        }
+
+        Ok(ConfigurationResolverFromMarkers::new(markers_map))
+    }
+}
+#[cfg(test)]
+mod test {
+    use mithril_cardano_node_chain::{
+        entities::{TxDatum, TxDatumBuilder, TxDatumFieldValue},
+        test::double::FakeChainObserver,
+    };
+    use mithril_common::{crypto_helper::ProtocolConfigurationMarkersSigner, entities::Epoch};
+
+    use crate::cardano_chain::{
+        ProtocolConfigurationMarkersPayloadCardanoChain,
+        SignedProtocolConfigurationMarkersPayloadCardanoChain,
+        message::ProtocolConfigurationMarker,
+    };
+
+    use crate::test::helper::{generate_configuration, generate_configuration_message};
+
+    use super::*;
+
+    fn dummy_tx_datums_from_markers_payload(
+        payloads: Vec<SignedProtocolConfigurationMarkersPayloadCardanoChain>,
+    ) -> Vec<TxDatum> {
+        payloads
+            .into_iter()
+            .map(|payload| {
+                TxDatumBuilder::new()
+                    .add_field(TxDatumFieldValue::Bytes(payload.to_json_hex().unwrap()))
+                    .build()
+                    .unwrap()
+            })
+            .collect()
+    }
+
+    #[tokio::test]
+    async fn test_cardano_chain_reader() {
+        let conf_a = generate_configuration_message('A');
+        let conf_b = generate_configuration_message('B');
+        let conf_c = generate_configuration_message('C');
+        let conf_d = generate_configuration_message('D');
+
+        let markers_signer = ProtocolConfigurationMarkersSigner::create_deterministic_signer();
+        let bad_markers_signer =
+            ProtocolConfigurationMarkersSigner::create_non_deterministic_signer();
+
+        let fake_address = "addr_test_123456".to_string();
+        let payload_1 = ProtocolConfigurationMarkersPayloadCardanoChain {
+            markers: vec![
+                ProtocolConfigurationMarker::new(Epoch(41), conf_a.to_cbor_hex().unwrap()),
+                ProtocolConfigurationMarker::new(Epoch(42), conf_b.to_cbor_hex().unwrap()),
+            ],
+        }
+        .sign(&bad_markers_signer)
+        .unwrap();
+
+        let payload_2 = ProtocolConfigurationMarkersPayloadCardanoChain {
+            markers: vec![
+                ProtocolConfigurationMarker::new(Epoch(43), conf_c.to_cbor_hex().unwrap()),
+                ProtocolConfigurationMarker::new(Epoch(44), conf_d.to_cbor_hex().unwrap()),
+            ],
+        }
+        .sign(&markers_signer)
+        .unwrap();
+
+        let mut fake_datums = dummy_tx_datums_from_markers_payload(vec![payload_2, payload_1]);
+
+        fake_datums.push(TxDatum("not_valid_datum".to_string()));
+
+        let chain_observer = FakeChainObserver::default();
+        chain_observer.set_datums(fake_datums.clone()).await;
+
+        let cardano_chain_reader = CardanoChainProtocolConfigurationMarkersReader::new(
+            fake_address,
+            Arc::new(chain_observer),
+            markers_signer.create_verifier().to_verification_key(),
+        );
+        let configurations = cardano_chain_reader
+            .read_configuration_markers()
+            .await
+            .expect("read_configuration_markers should not fail");
+
+        assert_eq!(2, configurations.markers.len());
+        assert_eq!(
+            Some(&generate_configuration('C')),
+            configurations.markers.get(&Epoch(43))
+        );
+        assert_eq!(
+            Some(&generate_configuration('D')),
+            configurations.markers.get(&Epoch(44))
+        );
+    }
+
+    #[tokio::test]
+    async fn test_cardano_chain_reader_should_throw_error_on_invalid_cbor() {
+        let markers_signer = ProtocolConfigurationMarkersSigner::create_deterministic_signer();
+        let fake_address = "addr_test_123456".to_string();
+        let payload_1 = ProtocolConfigurationMarkersPayloadCardanoChain {
+            markers: vec![
+                ProtocolConfigurationMarker::new(
+                    Epoch(41),
+                    generate_configuration_message('A').to_cbor_hex().unwrap(),
+                ),
+                ProtocolConfigurationMarker::new(Epoch(42), "unvalid_cbor_hex".to_string()),
+            ],
+        }
+        .sign(&markers_signer)
+        .unwrap();
+
+        let fake_datums = dummy_tx_datums_from_markers_payload(vec![payload_1]);
+
+        let chain_observer = FakeChainObserver::default();
+        chain_observer.set_datums(fake_datums.clone()).await;
+
+        let cardano_chain_reader = CardanoChainProtocolConfigurationMarkersReader::new(
+            fake_address,
+            Arc::new(chain_observer),
+            markers_signer.create_verifier().to_verification_key(),
+        );
+        let error = cardano_chain_reader
+            .read_configuration_markers()
+            .await
+            .expect_err("should throw error");
+
+        assert!(error.to_string().contains(
+            "ProtocolConfigurationForEpochMessage for Epoch(42) could not be decoded from cbor hex:"
+        ));
     }
 }
