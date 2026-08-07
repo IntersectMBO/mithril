37a4768d5ca76d511abdadfd54bac729ac252ce6
Clément Turmel <turmel@teragone-solutions.com>
2026-08-07 16:03:43 +0200

    feature(protocol-config): Implement a protocol configuration builder

--- internal/mithril-protocol-config/src/lib.rs
+++ internal/mithril-protocol-config/src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(missing_docs)]
 //! This crate provides mechanisms to read and check the configuration parameters of a Mithril network.
 
+pub mod builder;
 pub mod cardano_chain;
 pub mod http;
 pub mod interface;
