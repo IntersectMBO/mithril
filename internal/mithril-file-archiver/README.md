df51d76e7808f6ef6e65ccda0caa05f355e0b344
DJO <790521+Alenar@users.noreply.github.com>
2026-08-12 19:02:37 +0200

    chore: scaffold `internal/mithril-file-archiver` crate

---
+++ internal/mithril-file-archiver/README.md
@@ -0,0 +1,10 @@
+# Mithril-file-archiver
+
+**This is a work in progress** 🛠
+
+An API to generate tar archives from files, directories, or serializable data (leveraging serde).
+
+Produced archives are byte-stable across systems as long as the following invariants do not change:
+
+- The version of the zstandard compression library
+- The parameters of the zstandard compression
---
+++ internal/mithril-file-archiver/src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(missing_docs)]
+
+//! # Mithril-file-archiver
+//!
+//! An API to generate tar archives from files, directories, or serializable data (leveraging serde).
+//!
+//! Produced archives are byte-stable across systems as long as the following invariants do not change:
+//! * The version of the zstandard compression library
+//! * The parameters of the zstandard compression
+//!
