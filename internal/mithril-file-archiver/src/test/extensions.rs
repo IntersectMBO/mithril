dc8b0c0af1a6b32223c1dccf8a26dd947744ca53
DJO <790521+Alenar@users.noreply.github.com>
2026-08-12 20:38:21 +0200

    refactor(aggregrator): remove unused `tar` and `zstd` dependencies and centralize `unpack_archive` test utility function

--- internal/mithril-file-archiver/src/test/extensions.rs
+++ internal/mithril-file-archiver/src/test/extensions.rs
@@ -1,12 +1,9 @@
-use std::fs::File;
 use std::path::{Path, PathBuf};
-
-use tar::Archive;
-use zstd::stream::read::Decoder;
 
 use mithril_common::entities::CompressionAlgorithm;
 
 use crate::FileArchive;
+use crate::test::unpack_archive;
 
 /// Extension trait adding test utilities to [FileArchive]
 pub trait FileArchiveTestExtension {
@@ -22,13 +19,10 @@ impl FileArchiveTestExtension for FileArchive {
             panic!("Only Zstandard compression is supported");
         }
 
-        let parent_dir = parent_dir.as_ref();
-        let file_tar_zst = File::open(self.get_file_path()).unwrap();
-        let file_tar_zst_decoder = Decoder::new(file_tar_zst).unwrap();
-        let mut archive = Archive::new(file_tar_zst_decoder);
-        let unpack_path = parent_dir.join("unpack");
+        let unpack_path = parent_dir.as_ref().join("unpack");
         std::fs::create_dir(&unpack_path).unwrap();
-        archive.unpack(&unpack_path).unwrap();
+
+        unpack_archive(self.get_file_path(), &unpack_path).unwrap();
 
         unpack_path
     }
