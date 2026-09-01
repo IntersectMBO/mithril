ec5b9da0763b12bbd3d2ce25f9e252b16b6ef317
Hamza Jeljeli <hamza.jeljeli@iohk.io>
2026-09-01 09:57:47 +0900

    refactor(stm): remove the stale dead-code markers from the IVC circuit and proof error

--- mithril-stm/src/proof_system/ivc_halo2_snark/errors.rs
+++ mithril-stm/src/proof_system/ivc_halo2_snark/errors.rs
@@ -1,7 +1,5 @@
 use thiserror::Error;
 
-// TODO: remove this allow dead_code when IvcProof::verify is called from non-test code
-#[allow(dead_code)]
 #[derive(Debug, Error, Clone, PartialEq, Eq)]
 pub(crate) enum IvcProofError {
     /// Transcript could not be prepared for KZG opening verification.
