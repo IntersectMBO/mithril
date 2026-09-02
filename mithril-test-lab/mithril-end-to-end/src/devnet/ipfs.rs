988c6ab1f58ff7a9c5e78651a0fbf8e5153117e5
DJO <790521+Alenar@users.noreply.github.com>
2026-09-02 20:57:43 +0200

    feat(e2e): ipfs devnet runner first draft

---
+++ mithril-test-lab/mithril-end-to-end/src/devnet/ipfs.rs
@@ -0,0 +1,152 @@
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+use std::process::Stdio;
+
+use anyhow::{Context, anyhow};
+use reqwest::Url;
+use slog_scope::info;
+use tokio::process::Command;
+
+use mithril_common::StdResult;
+
+use crate::RetryableDevnetError;
+use crate::utils::{ChildLoggerExt, file_utils};
+
+const IPFS_DEVNET_SCRIPT_NAME: &str = "ipfs-devnet.sh";
+
+#[derive(Debug, Clone, Default)]
+pub struct IpfsDevnet {
+    devnet_script_path: PathBuf,
+    swarm_dir: PathBuf,
+    topology: Vec<KuboNode>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct KuboNode {
+    pub rpc_url: Url,
+    pub working_dir: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+pub struct IpfsDevnetBootstrapArgs {
+    pub devnet_scripts_dir: PathBuf,
+    pub number_of_nodes: u8,
+    pub swarm_target_dir: PathBuf,
+    pub kubo_version: Option<semver::Version>,
+}
+
+impl IpfsDevnet {
+    fn build_command<C: AsRef<OsStr>>(&self, sub_command: C) -> StdResult<Command> {
+        let mut command = Command::new(self.devnet_script_path.clone());
+        command
+            .arg(sub_command)
+            .env("SWARM_DIR", self.swarm_dir.as_os_str())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .kill_on_drop(true);
+        Ok(command)
+    }
+
+    fn build_topology(number_of_nodes: u8, swarm_dir: &Path) -> Vec<KuboNode> {
+        (1..=number_of_nodes)
+            .map(|n| KuboNode {
+                rpc_url: Url::parse(&format!("http://127.0.0.1:{}", 5000_u16 + n as u16)).unwrap(),
+                working_dir: swarm_dir.join(format!("kubo-node-{n}")),
+            })
+            .collect()
+    }
+
+    pub async fn bootstrap(bootstrap_args: &IpfsDevnetBootstrapArgs) -> StdResult<IpfsDevnet> {
+        let devnet_script_path = file_utils::get_process_path(
+            IPFS_DEVNET_SCRIPT_NAME,
+            &bootstrap_args.devnet_scripts_dir,
+        )?;
+        let mut bootstrap_command = Command::new(&devnet_script_path);
+        bootstrap_command
+            .arg("init")
+            .arg("--overwrite")
+            .arg("--number")
+            .arg(bootstrap_args.number_of_nodes.to_string())
+            .arg("--swarm-dir")
+            .arg(bootstrap_args.swarm_target_dir.as_os_str());
+
+        if let Some(kubo_version) = &bootstrap_args.kubo_version {
+            bootstrap_command.env("KUBO_VERSION", kubo_version.to_string());
+        }
+
+        bootstrap_command
+            .current_dir(&bootstrap_args.devnet_scripts_dir)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .kill_on_drop(true);
+
+        info!("Bootstrapping the IPFS Devnet"; "script" => &devnet_script_path.display(), "cmd" => "init");
+
+        let exit_status = bootstrap_command
+            .spawn()
+            .with_context(|| format!("{IPFS_DEVNET_SCRIPT_NAME} failed to start"))?
+            .wait_forwarding_output_to_slog_scope(IPFS_DEVNET_SCRIPT_NAME)
+            .await
+            .with_context(|| format!("{IPFS_DEVNET_SCRIPT_NAME} failed to run"))?;
+        match exit_status.code() {
+            Some(0) => Ok(IpfsDevnet {
+                devnet_script_path,
+                swarm_dir: bootstrap_args.swarm_target_dir.to_owned(),
+                topology: Self::build_topology(
+                    bootstrap_args.number_of_nodes,
+                    &bootstrap_args.swarm_target_dir,
+                ),
+            }),
+            Some(code) => Err(anyhow!(RetryableDevnetError(format!(
+                "IPFS Bootstrap devnet exited with status code: {code}"
+            )))),
+            None => Err(anyhow!("IPFS Bootstrap devnet terminated by signal")),
+        }
+    }
+
+    pub fn swarm_dir(&self) -> PathBuf {
+        self.swarm_dir.clone()
+    }
+
+    pub fn topology(&self) -> &[KuboNode] {
+        &self.topology
+    }
+
+    pub async fn start(&self) -> StdResult<()> {
+        let mut run_command = self.build_command("start")?;
+
+        info!("Starting the IPFS devnet"; "script" => &self.devnet_script_path.display(), "cmd" => "start");
+
+        let status = run_command
+            .spawn()
+            .with_context(|| "Failed to start the IPFS devnet")?
+            .wait_forwarding_output_to_slog_scope(&format!("{IPFS_DEVNET_SCRIPT_NAME} start"))
+            .await
+            .with_context(|| "Error while starting the IPFS devnet")?;
+        match status.code() {
+            Some(0) => Ok(()),
+            Some(code) => Err(anyhow!(RetryableDevnetError(format!(
+                "Run IPFS devnet exited with status code: {code}"
+            )))),
+            None => Err(anyhow!("Run IPFS devnet terminated by signal")),
+        }
+    }
+
+    pub async fn stop(&self) -> StdResult<()> {
+        let mut run_command = self.build_command("stop")?;
+
+        info!("Stopping the IPFS devnet"; "script" => &self.devnet_script_path.display(), "cmd" => "stop");
+
+        let exit_status = run_command
+            .spawn()
+            .with_context(|| "Failed to stop the IPFS devnet")?
+            .wait_forwarding_output_to_slog_scope(&format!("{IPFS_DEVNET_SCRIPT_NAME} stop"))
+            .await
+            .with_context(|| "Error while stopping the IPFS devnet")?;
+        match exit_status.code() {
+            Some(0) => Ok(()),
+            Some(code) => Err(anyhow!("Stop IPFS devnet exited with status code: {code}")),
+            None => Err(anyhow!("Stop IPFS devnet terminated by signal")),
+        }
+    }
+}
--- mithril-test-lab/mithril-end-to-end/src/devnet/mod.rs
+++ mithril-test-lab/mithril-end-to-end/src/devnet/mod.rs
@@ -1,6 +1,8 @@
 mod cardano;
+mod ipfs;
 
 pub use cardano::{Devnet, DevnetBootstrapArgs, DevnetTopology, FullNode, PoolNode};
+pub use ipfs::{IpfsDevnet, IpfsDevnetBootstrapArgs, KuboNode};
 
 #[derive(thiserror::Error, Debug, PartialEq, Eq)]
 #[error("Retryable devnet error: `{0}`")]
