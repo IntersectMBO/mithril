12fa31dfb65e283c0a720a274a28777e7363fe38
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2026-09-16 15:50:55 +0200

    feat: upgrade Cardano default from '11.0.1' to '11.1.2'

--- mithril-infra/variables.tf
+++ mithril-infra/variables.tf
@@ -173,7 +173,7 @@ locals {
 variable "cardano_image_id" {
   type        = string
   description = "The Cardano image tag of service to deploy"
-  default     = "10.7.1"
+  default     = "11.1.2"
 }
 
 variable "cardano_image_registry" {
